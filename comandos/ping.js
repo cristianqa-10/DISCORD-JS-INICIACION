63f52b43eec1da122f138853af97acbdb9ac5de1
cristianqa-10 <epogonzalo26@gmail.com>
2024-12-18 22:22:31 +0100

    Update ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,1 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Responde con Pong!'),
+    async execute(interaction) {
+        await interaction.reply('🏓 Pong!');
+    },
+};
